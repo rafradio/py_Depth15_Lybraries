c375d1d691fe53aef57986903e3baeea6aae0199
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-27 11:19:36 +0300

    logger

---
+++ MyLogger.py
@@ -0,0 +1,18 @@
+import logging, inspect
+
+class MyLogger:
+    logger: logging.Logger = None
+    
+    @staticmethod
+    def configure():
+        FORMAT = '%(clientip)-15s %(name)-4s %(message)s'
+        # logging.basicConfig(format=FORMAT)
+        logger_name = inspect.stack()[1][3]
+        handler = logging.FileHandler('app.log', 'w', 'utf-8')
+        logger = logging.getLogger('taskData')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(FORMAT)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        MyLogger.logger = logger
+   
